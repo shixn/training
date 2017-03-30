2ba25a01e49e3d58355475a59886c928b5bd4331
Shin Hasegawa <S_HASEGAWA@hasegawa-mpb-2.local>
2017-03-30 17:36:39 +0900

    [update] Add article sentense to index.php.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
-Hello github and heroku!!
-
-github and heroku is very useful
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>circleCI実装テスト</title>
+</head>
+<body>
+  <article>
+    <h1>circleCI実装テスト</h1>
+    <h2>実際に静的サイトの構築の際にcircleCIを実装してみます</h2>
+  </article>
+</body>
+</html>
